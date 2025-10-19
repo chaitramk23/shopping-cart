ba8badf233e6c5b0af2b487503ef0ad89503707a
chaitramk <cmk6790@gmail.com>
2025-10-19 19:18:22 +0530

    Update main.tf

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -1,3 +1,42 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create key pair
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "jenkins-deploy-key"
+  public_key = tls_private_key.key.public_key_openssh
+}
+
+# Security group
+resource "aws_security_group" "sg" {
+  name        = "tomcat-sg"
+  description = "Allow SSH and Tomcat"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 # EC2 instance
 data "aws_ami" "amazon_linux" {
   most_recent = true
@@ -27,3 +66,4 @@ output "private_key_pem" {
   value     = tls_private_key.key.private_key_pem
   sensitive = true
 }
+
